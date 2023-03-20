0338475b0e35df54407075641cec10f87ec52bc4
EC2 Default User <ec2-user@ip-172-31-46-23.ap-northeast-1.compute.internal>
2023-03-20 01:24:49 +0000

    AddColumn

--- db/migrate/20230319023545_create_items.rb
+++ db/migrate/20230319023545_create_items.rb
@@ -1,12 +1,10 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
-
       t.string :name, null: false
       t.string :explanation, null: false
       t.integer :price, null: false
       t.integer :selling_status, default: 0, null: false
-      
 
       t.timestamps
     end
