7c408d67495067b8be1b6d0aa92eb051376c2fb6
Tomoyuki123Kawabata <122082755+Tomoyuki123Kawabata@users.noreply.github.com>
2023-03-20 19:48:07 +0900

    Revert "merge now"

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :customers,skip:[:password],controllers:{
-    registriations:"public/registrations",
-    sessions:'public/sessions'
-  }
-  
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-    sessions: "admin/sessions"
-  }
-  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
