4711459edaae4a4c7e20825c603b900f9085fa29
Tomoyuki123Kawabata <122082755+Tomoyuki123Kawabata@users.noreply.github.com>
2023-03-21 06:37:57 +0000

    [ADD]担当分のルートを変更

--- config/routes.rb
+++ config/routes.rb
@@ -24,15 +24,18 @@ devise_for :customers,skip:[:password],controllers:{
   get "home/about"=>"homes#about"
   get "search" => "searches#search"
 
- namespace :customers do
+
    resources :customers, only: [:show, :edit] do
      get :unsubscribe
      get :withdraw
    end
    resources :orders, only: [:new, :confirm, :finish, :index, :show]
- end
-
-
-
+   resources :addresses, only: [:index, :edit]
+   resources :cart_items, only: [:index] do
+    collection do
+     delete :destroy_all
+    end
+   end
+　 resources :items, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+ end 
