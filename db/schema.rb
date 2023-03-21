c79eb16b9ddb2084a5ade9bb170ae874bec06ce8
EC2 Default User <ec2-user@ip-172-31-27-249.ap-northeast-1.compute.internal>
2023-03-21 09:03:12 +0000

    ヘッダーの修正（まだ未完成）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_19_025528) do
+ActiveRecord::Schema.define(version: 2023_03_20_130821) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -114,7 +114,6 @@ ActiveRecord::Schema.define(version: 2023_03_19_025528) do
     t.string "address", null: false
     t.string "name", null: false
     t.integer "delivery_cost", default: 800, null: false
-    t.integer "payment_method", default: 0
     t.integer "order_status", default: 0
     t.integer "total_payment", null: false
     t.datetime "created_at", precision: 6, null: false
