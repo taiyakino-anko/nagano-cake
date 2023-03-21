ce6854e902b6d5837772a87e1c2331d4e6646344
Tomoyuki123Kawabata <122082755+Tomoyuki123Kawabata@users.noreply.github.com>
2023-03-21 06:59:28 +0000

    [ADD]アクション記述とルートファイル内に追記

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ devise_for :customers,skip:[:password],controllers:{
     resources :genres, only: [:index, :edit, :create, :update]
     resources :items, only: [:index, :new, :show, :edit, :create, :update]
     resources :orders, only: [:show, :update]
-    resources :order_details, only[:update]
+    resources :order_details, only: [:update]
     get 'homes/top'
   end
 
@@ -29,12 +29,12 @@ devise_for :customers,skip:[:password],controllers:{
      get :withdraw
    end
    resources :orders, only: [:new, :confirm, :finish, :index, :show]
-   resources :addresses, only: [:index, :edit]
-   resources :cart_items, only: [:index] do
+   resources :addresses, only: [:index, :edit,:create,:update,:destroy]
+   resources :cart_items, only: [:index,:update,:destroy,:create] do
     collection do
      delete :destroy_all
     end
    end
-　 resources :items, only: [:index, :show]
+   resources :items, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- end 
+ end
