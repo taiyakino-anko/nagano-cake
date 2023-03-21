ad8b5d98d80ec5f2e1df00456ad206eb360027cd
EC2 Default User <ec2-user@ip-172-31-27-249.ap-northeast-1.compute.internal>
2023-03-21 06:56:20 +0000

    routes.rbのエラー修正とcreate,updateアクションの追加（岡村分）

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ devise_for :customers,skip:[:password],controllers:{
     resources :genres, only: [:index, :edit, :create, :update]
     resources :items, only: [:index, :new, :show, :edit, :create, :update]
     resources :orders, only: [:show, :update]
-    resources :order_details, only[:update]
+    resources :order_details, only: [:update]
     get 'homes/top'
   end
 
@@ -24,17 +24,17 @@ devise_for :customers,skip:[:password],controllers:{
   get "home/about"=>"homes#about"
   get "search" => "searches#search"
 
-   resources :customers, only: [:show, :edit] do
+   resources :customers, only: [:show, :edit, :update] do
      get :unsubscribe
      get :withdraw
    end
-   resources :orders, only: [:new, :confirm, :finish, :index, :show]
+   resources :orders, only: [:new, :confirm, :finish, :index, :show, :create]
    resources :addresses, only: [:index, :edit]
    resources :cart_items, only: [:index] do
     collection do
      delete :destroy_all
     end
    end
-　 resources :items, only: [:index, :show]
+   resources :items, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- end 
+ end
