b14797ff1a91c2d1873fb3f47240657ca30d79c9
EC2 Default User <ec2-user@ip-172-31-27-249.ap-northeast-1.compute.internal>
2023-03-22 09:09:05 +0000

    トップとアバウトページの作成（未完成）

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,16 +1,17 @@
 class Public::CustomersController < ApplicationController
+  # マイページ
   def show
   end
-
+  # 会員情報編集
   def edit
   end
-
+  # 退会確認
   def unsubscribe
   end
-
+  # 退会処理（ステータス更新）
   def withdraw
   end
-
+  # 会員情報の更新
   def update
   end
 end
