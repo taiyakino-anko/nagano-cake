09354af2ecde295edb9f06b165dae4f04bce8c4d
EC2 Default User <ec2-user@ip-172-31-27-249.ap-northeast-1.compute.internal>
2023-03-22 18:05:52 +0000

    退会確認画面の作成_ver2

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -11,6 +11,10 @@ class Public::CustomersController < ApplicationController
   end
   # 退会処理（ステータス更新）
   def withdraw
+    @customer = Customer.find(params[:id])
+    @customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
   end
   # 会員情報の更新
   def update
