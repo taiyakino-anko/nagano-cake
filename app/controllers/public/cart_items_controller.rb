9399fa3f4360226579bbf5f7d5548aa50df80dcf
Tomoyuki123Kawabata <122082755+Tomoyuki123Kawabata@users.noreply.github.com>
2023-03-23 12:54:18 +0000

    [ADD]カート機能の実装

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,16 +1,37 @@
 class Public::CartItemsController < ApplicationController
+  #before_action :authenticate_customer!
+  
   def index
-  end
-  def update
-    
-  end
-  def create
-    
-  end
-  def destroy
-    
+    @cart_items = current_customer.cart_items
+    @total_price = @cart_items.sum(&:subtotal)
   end
   
+  def update
+
+  end
+  def create
+    #追加した商品がカート内に存在するかの判別
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      #存在した場合
+      #カート内の個数をフォームから送られた個数分追加する
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.save
+    else 
+      @cart_item = CartItem.new
+    end
+  end
+  
+  def destroy
+
+  end
+
   def destroy_all
+    @cart_item = current_customer.cart_items.destroy_all
+  end
+
+  private
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :amount)
   end
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,13 @@
 class CartItem < ApplicationRecord
   belongs_to :items
   belongs_to :customers
+  
+  
+  def with_tax_price
+    (price * 1.1).floor
+  end
+  
+  def subtotal
+    item.with_tax_price * amount
+  end
 end
