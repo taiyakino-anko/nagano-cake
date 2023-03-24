a192ff457dcb3502a1ec38d1744e00cd7044d33b
EC2 Default User <ec2-user@ip-172-31-46-23.ap-northeast-1.compute.internal>
2023-03-24 08:12:05 +0000

    develop3

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,16 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.all
   end
 
   def show
+    @customer = Customer.find
   end
 
   def edit
+  end
+  
+  def update
+    
   end
 end
