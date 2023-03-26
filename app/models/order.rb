f11519073b839ee4099aeae483949a0fd22798ad
EC2 Default User <ec2-user@ip-172-31-27-249.ap-northeast-1.compute.internal>
2023-03-26 04:45:53 +0000

    enum設定完了

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,16 @@ class Order < ApplicationRecord
 
   has_many :items, through: :order_details
   belongs_to :customer
+
+  enum order_status: {
+    "入金待ち": 0,
+    "入金確認中": 1,
+    "製作中": 2,
+    "発送準備中": 3,
+    "発送済み": 4
+  }
+  enum payment_method: {
+    "クレジットカード": 0,
+    "銀行振込": 1
+  }
 end
